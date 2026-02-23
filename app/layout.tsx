f9f99946f66b914166a3ba69e881a7382b365325
Hà Dopaaa <ha0989916807@gmail.com>
2026-02-23 20:56:04 +0700

    svg logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
     title: "zenjichen music box",
     description: "Stream your personal music collection from Google Drive",
     icons: {
-        icon: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🎵</text></svg>',
+        icon: '/icon.jpg',
     },
 };
 
@@ -21,9 +21,11 @@ export const viewport: Viewport = {
 
 const MusicLogoIcon = () => (
     <div className="logo-icon-wrapper">
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="logo-svg">
-            <path d="M12 3V13.55C11.41 13.21 10.73 13 10 13C7.79 13 6 14.79 6 17C6 19.21 7.79 21 10 21C12.21 21 14 19.21 14 17V7H18V3H12Z" fill="currentColor" />
-        </svg>
+        <img
+            src="/icon.jpg"
+            alt="zenjichen music box"
+            className="logo-img"
+        />
         <div className="logo-sparkle"></div>
     </div>
 );
